567ae42fdd9f1090c73678de847480b44524e2aa
Aarti <aartr16@gmail.com>
2022-06-28 14:41:38 -0500

    Initial Commit

---
+++ KALA kwk 2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KALA kwk 2022
+//
+//  Created by Scholar on 6/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
